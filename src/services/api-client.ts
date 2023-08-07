82b059b0d6980dfd41ee03ae22abfbbbed995542
Michael Obiero <obimike8724@gmail.com>
2023-08-07 20:54:00 +0300

    Fetch the games

---
+++ src/services/api-client.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+export default axios.create({
+    baseURL: "https://api.rawg.io/api",
+    params: {
+        key: "837cb64017e7488ea4d12ea6db57a37d",
+    },
+
+});
+
+
+
+// Storing the API key for rawg.io in this source code and committing it to Git (approach) 
+// is a temporary solution and not the recommended way to manage API keys.
